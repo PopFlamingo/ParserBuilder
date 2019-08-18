d8c5459a6791c0c1345f041ad1d55a6440de9185
Trevör Anne Denise <trevor.annedenise@icloud.com>
2019-08-18 20:09:50 +0200

    Add peek and pop to extractor

--- Sources/ParserBuilder/Extractor.swift
+++ Sources/ParserBuilder/Extractor.swift
@@ -1,11 +1,12 @@
 public struct Extractor {
     public init(_ string: String) {
         self.string = string
+        self.currentIndex = string.startIndex
     }
     private let string: String
-    public func matches(for matcher: Matcher) -> [Substring] {
+    private var currentIndex: String.Index
+    mutating public func matches(for matcher: Matcher) -> [Substring] {
         var matches = [Substring]()
-        var currentIndex = string.startIndex
         while currentIndex < string.endIndex {
             if let nextIndex = matcher.advancedIndex(in: string[currentIndex...]) {
                 matches.append(string[currentIndex..<nextIndex])
@@ -15,5 +16,31 @@ public struct Extractor {
             }
         }
         return matches
+    }
+    
+    public mutating func peekCurrent(with matcher: Matcher) -> Substring? {
+        guard currentIndex < string.endIndex else {
+            return nil
+        }
+        if let endIndex = matcher.advancedIndex(in: string[currentIndex...]) {
+            return string[currentIndex..<endIndex]
+        } else {
+            return nil
+        }
+    }
+    
+    @discardableResult
+    public mutating func popCurrent(with matcher: Matcher) -> Substring? {
+        guard currentIndex < string.endIndex else {
+            return nil
+        }
+        if let endIndex = matcher.advancedIndex(in: string[currentIndex...]) {
+            defer {
+                currentIndex = endIndex
+            }
+            return string[currentIndex..<endIndex]
+        } else {
+            return nil
+        }
     }
 }
