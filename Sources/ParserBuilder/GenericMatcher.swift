578b98c2f6e41ec74760ae010dc5db2d28546c3f
Trevör Anne Denise <trevor.annedenise@icloud.com>
2019-09-03 17:57:37 +0200

    Add @inlinable on some functions

--- Sources/ParserBuilder/GenericMatcher.swift
+++ Sources/ParserBuilder/GenericMatcher.swift
@@ -219,6 +219,7 @@ public struct GenericMatcher<C>: ExpressibleByArrayLiteral where C: Collection, 
 
 
 extension String {
+    @inlinable
     var utf8Characters: [UInt8] {
         var copy = self
         return copy.withUTF8({ Array($0) })
@@ -227,6 +228,7 @@ extension String {
 
 extension GenericMatcher where C == String {
     
+    @inlinable
     func toOptimizedASCII() -> GenericMatcher<[UInt8]>? {
         switch self.matcher {
         
